acc0ee9b8d92d2ee10d2a9a0acdd1de993c7cc09
GuningShen <gunings@email.unc.edu>
2022-11-26 11:52:10 -0500

    first check

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
-const express = require("express")
-const minimist = require("minimist")
+#!/usr/bin/env node
 
+import {roll} from './lib/roll.js'
+import express from 'express';
+import minimist from 'minimist';
 
 const app = express()
+const args = minimist(process.argv.slice(2))
+app.use(express.urlencoded({ extended: true}));
 
-app.get("/app", function(req, res) {
-    console.log(res.statusCode)
+app.get("/app/", function(req, res) {
+    res.send('200 OK')
 })
 
-app.get("/app/roll", function(req, res) {
-
+app.get("/app/roll/", function(req, res) {
+    var sides = 6;
+    var dice = 2;
+    var rolls = 1;
+    var result = roll(sides, dice, rolls)
+    res.status(200).send(result)
 })
 
-app.use((req, res, next) => {
-    res.status(404).send(
-        "<h1>Page not found on the server</h1>")
+app.post("/app/roll/", function(req, res) {
+    var sides = parseInt(req.body.sides);
+    var dice = parseInt(req.body.dice);
+    var rolls = parseInt(req.body.rolls);
+    var result = roll(sides, dice, rolls)
+    res.status(200).send(result)
 })
+
+app.post("/app/roll/:sides/", function(req, res) {
+    var sides = parseInt(req.params.sides);
+    var result = roll(sides, 2, 1)
+    res.status(200).send(result)
+})
+
+app.post("/app/roll/:sides/:dice/", function(req, res){
+    var sides = parseInt(req.params.sides);
+    var dice = parseInt(req.params.dice);
+    var result = roll(sides, dice, 1)
+    res.status(200).send(result)
+})
+
+app.post("/app/roll/:sides/:dice/:rolls/", function(req, res){
+    var sides = parseInt(req.params.sides);
+    var dice = parseInt(req.params.dice);
+    var rolls = parseInt(req.params.rolls);
+    var result = roll(sides, dice, rolls)
+    res.status(200).send(result)
+})
+
+app.use(function(req, res){
+	res.status(404).send("404 NOT FOUND");
+});
 
 var port = 5000
-app.listen(port)
+if (args.port) {
+    port = args.port
+}
+app.listen(port, function() {
+    console.log("Server is listening on port " +port +".")
+})
